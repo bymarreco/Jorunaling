e77ed38631b0634f679929eb27e8afc7a4de4dfd
Lucas Rech <bymarreco@gmail.com>
2021-03-15 20:51:43 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Journaling.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Question.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Journaling.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string Descricao { get; set; }
+    }
+}
